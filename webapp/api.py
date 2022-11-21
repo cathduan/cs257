30ce2a2c3c2073be005f61ebec18991b90549704
cathduan <113489736+cathduan@users.noreply.github.com>
2022-11-20 21:19:29 -0600

    Update api.py

--- webapp/api.py
+++ webapp/api.py
@@ -10,6 +10,8 @@ import json
 import config
 import psycopg2
 import re
+import urllib.request
+# from met_image_url_scraper import *
 
 api = flask.Blueprint('api', __name__)
 
@@ -20,6 +22,18 @@ def get_connection():
     except Exception as e:
         print(e)
         exit()
+
+def image_url(page_url):
+    ''' Given the URL for one of the Met's single-artwork pages, retrieve
+        that page, extract the artwork's image url, and return that url.
+        If this fails, return an empty string. '''
+    data_from_server = urllib.request.urlopen(page_url).read()
+    string_from_server = data_from_server.decode('utf-8')
+    match = re.search(r'<img id="artwork__image".*src="([^"]*)"', string_from_server)
+    print(string_from_server)
+    if match:
+        return match.group(1)
+    return ''
 
 @api.route('/browse-all')
 def browse_all():
@@ -82,7 +96,7 @@ def browse_all():
         print(e, file=sys.stderr)
     
     query = '''SELECT artworks.id, artworks.artwork, artworks.art_date, artworks.material_id, artworks.artist_id, artworks.geographic_location_id, artworks.department_id, artists.id, artists.artist_firstname, artists.artist_surname, materials.id, materials.material_type,
-                collections.id, collections.department_name, geographic_locations.id, geographic_locations.country_name
+                collections.id, collections.department_name, geographic_locations.id, geographic_locations.country_name, artworks.link_resource
                 FROM artworks, artists, materials, collections, geographic_locations
                 WHERE artists.id = artworks.artist_id
                 AND materials.id = artworks.material_id
@@ -100,7 +114,9 @@ def browse_all():
                         'art_date':row[2],
                         'artist_firstname': row[8],
                         'artist_surname':row[9],
-                        'material_type':row[11]}
+                        'material_type':row[11],
+                        'link_resource': row[16]}
+            # print(image_url(row[16]) + "test!")
             current_browsing.append(current)
         cursor.close()
        
@@ -118,7 +134,7 @@ def browse_all_collections(collection_name):
     collection_name = " ".join(collection_name_list)
     collection_name_search = "%" + collection_name + "%"
     query = '''SELECT artworks.id, artworks.artwork, artworks.art_date, artworks.material_id, artworks.artist_id, artworks.geographic_location_id, artworks.department_id, artists.id, artists.artist_firstname, artists.artist_surname, materials.id, materials.material_type,
-                collections.id, collections.department_name, geographic_locations.id, geographic_locations.country_name
+                collections.id, collections.department_name, geographic_locations.id, geographic_locations.country_name, artworks.link_resource
                 FROM artworks, artists, materials, collections, geographic_locations
                 WHERE artists.id = artworks.artist_id
                 AND materials.id = artworks.material_id
@@ -157,7 +173,7 @@ def browse_all_geographic_locations(geographic_location_name):
     geographic_location_name_search = "%" + geographic_location_name + "%"
     print(geographic_location_name)
     query = '''SELECT artworks.id, artworks.artwork, artworks.art_date, artworks.material_id, artworks.artist_id, artworks.geographic_location_id, artworks.department_id, artists.id, artists.artist_firstname, artists.artist_surname, materials.id, materials.material_type,
-                collections.id, collections.department_name, geographic_locations.id, geographic_locations.country_name
+                collections.id, collections.department_name, geographic_locations.id, geographic_locations.country_name, artworks.link_resource
                 FROM artworks, artists, materials, collections, geographic_locations
                 WHERE artists.id = artworks.artist_id
                 AND materials.id = artworks.material_id
@@ -196,21 +212,61 @@ def browse_all_materials(material_name):
     print(material_name)
     material_name_search = "%" + material_name + "%"
     query = '''SELECT artworks.id, artworks.artwork, artworks.art_date, artworks.material_id, artworks.artist_id, artworks.geographic_location_id, artworks.department_id, artists.id, artists.artist_firstname, artists.artist_surname, materials.id, materials.material_type,
-                collections.id, collections.department_name, geographic_locations.id, geographic_locations.country_name
+                collections.id, collections.department_name, geographic_locations.id, geographic_locations.country_name, artworks.link_resource
                 FROM artworks, artists, materials, collections, geographic_locations
                 WHERE artists.id = artworks.artist_id
                 AND materials.id = artworks.material_id
                 AND geographic_locations.id = artworks.geographic_location_id
                 AND collections.id = artworks.department_id
                 AND LOWER(materials.material_type) LIKE LOWER(%s)
-                ORDER BY artworks.artwork, artworks.id'''
+                ORDER BY artworks.artwork, artworks.id LIMIT 3000'''
 
     current_browsing = []
-    print("test")
     try:
         connection = get_connection()
         cursor = connection.cursor()
         cursor.execute(query, (material_name_search,))
+        for row in cursor:
+            current = {'artwork':row[1],
+                        'art_date':row[2],
+                        'artist_firstname': row[8],
+                        'artist_surname':row[9],
+                        'material_type':row[11]}
+            # url = row[16]
+            # url = url[11:-1]
+            # print(url)
+            # print(image_url(url))
+            current_browsing.append(current)
+        cursor.close()
+       
+        connection.close()
+
+    except Exception as e:
+        print(e, file=sys.stderr)
+
+    browse_all_page = {'current_browsing' : current_browsing}
+    return json.dumps(browse_all_page)    
+
+@api.route('/browse-all/q/<query_name>')
+def browse_all_query(query_name):
+    query_name_list = re.split(', |_|-|! |\|', query_name)
+    query_name = " ".join(query_name_list)
+    query_name_search = "%" + query_name + "%"
+    query = '''SELECT artworks.id, artworks.artwork, artworks.art_date, artworks.material_id, artworks.artist_id, artworks.geographic_location_id, artworks.department_id, artists.id, artists.artist_firstname, artists.artist_surname, materials.id, materials.material_type,
+                collections.id, collections.department_name, geographic_locations.id, geographic_locations.country_name, artworks.link_resource
+                FROM artworks, artists, materials, collections, geographic_locations
+                WHERE artists.id = artworks.artist_id
+                AND materials.id = artworks.material_id
+                AND geographic_locations.id = artworks.geographic_location_id
+                AND collections.id = artworks.department_id
+                AND LOWER(artworks.artwork) LIKE LOWER(%s)
+                ORDER BY artworks.artwork, artworks.id'''
+
+    current_browsing = []
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(query, (query_name_search,))
         for row in cursor:
             current = {'artwork':row[1],
                         'art_date':row[2],
@@ -251,7 +307,7 @@ def get_collections():
     return json.dumps(collections_list)
 
 
-@api.route('/mockup7') 
+@api.route('/artists') 
 def get_artists():
     query = '''SELECT artist_surname, artist_firstname, artist_bio, artist_birthyear, artist_deathyear
                FROM artists 
@@ -281,4 +337,9 @@ def get_artists():
 
 @api.route('/help')
 def get_help():
-    return
+    file = open('api-design.txt')
+    lines = file.readlines()
+    help_text = ''
+    for line in lines:
+        help_text += (line)
+    return help_text
